2767adf01b879a6441ed061e592cc96a77f1d485
levakuz <levakuz007@gmail.com>
2020-02-05 17:20:38 +0300

    05.02.2020 Добавлена таблица в БД которая хранит значение UID RFID метки и ее физического номера, реализована отправка на кассу физического номера. Статус заказа нельзя менять если нет номера стола. Добавлена функция робота, отвечающая за передачу роботу значений True или False исходя из полученной от него RFID метки.

--- rfid_mqttt_sender.py
+++ rfid_mqttt_sender.py
@@ -3,37 +3,71 @@ import pika
 import time
 
 credentials = pika.PlainCredentials('guest', 'guest')
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.13',
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.1.100',
                                                                5672,
                                                                '/',
-                                                               credentials))
-
-
+                                                               credentials=credentials,
+                                                               # socket_timeout=None,
+                                                               # retry_delay=0,
+                                                               heartbeat=0,
+                                                               blocked_connection_timeout=300,
+                                                               ))
 channel = connection.channel()
-channel.queue_declare(queue='rfid', durable=True)
+channel.queue_declare(queue='rfidnums', durable=True)
 
 
-def on_message(client , userdata ,  message):
-    print("message received ", str(message.payload.decode("utf-8")))
-    # nomerok = input('Введите номер номерка: ')
-    new_message = str(message.payload.decode("utf-8"))
-    channel.basic_publish(
-        exchange='',
-        routing_key='rfid',
-        body=new_message,
-        properties=pika.BasicProperties(
-            delivery_mode=2,
-        ))
+properties = pika.BasicProperties(content_type='text/plain', delivery_mode=1)
+
+
+def send_to_rabbit(message):
+    try:
+        channel.basic_publish(
+         exchange='',
+         routing_key='rfidnums',
+         body=message,
+         properties=properties,
+         )
+        print(connection.is_open)
+    except:
+        print(connection.is_open)
+        print("didnt work")
+
+
+def on_message(client, userdata ,  message):
+    if message.topic == "rfids/":
+        print("message received ", str(message.payload.decode("utf-8")))
+        send_to_rabbit(str(message.payload.decode("utf-8")))
+    elif message.topic == "tables/":
+        new_message = str(message.payload.decode("utf-8"))
+        print("message received ", new_message)
+        channel.basic_publish(
+             exchange='',
+             routing_key='bdtables',
+             body=new_message
+             )
+    elif message.topic == "robots/":
+        new_message = str(message.payload.decode("utf-8"))
+        print("message received ", new_message)
+        channel.basic_publish(
+            exchange='',
+            routing_key='bdrobots',
+            body=new_message
+        )
     print("message topic=", message.topic)
     print("message qos=", message.qos)
     print("message retain flag=", message.retain)
 
 
-hostIP = "192.168.0.13"
-client = mqtt.Client("P2")
+hostIP = "192.168.1.100"
+client = mqtt.Client('P1', clean_session=True)
+# client.username_pw_set("mqtt-test", "mqtt-test")
 client.connect(hostIP)
 client.on_message = on_message
-client.subscribe("test/")
-time.sleep(2)
+client.subscribe("rfids/")
+client.subscribe("tables/")
+client.subscribe("robots/")
 client.loop_forever()
+
+
+
 
