65ac2af5dbe08b8aa199e83c93c0c4f4b603c3e9
levakuz <levakuz007@gmail.com>
2020-07-30 19:01:15 +0300

    30.07.2020

--- rfid_mqttt_sender.py
+++ rfid_mqttt_sender.py
@@ -61,7 +61,7 @@ def on_message(client, userdata ,  message):
 
 hostIP = 'rabbitmq'
 client = mqtt.Client('P1', clean_session=True)
-client.connect(hostIP)
+client.connect(hostIP, 1883)
 client.on_message = on_message
 client.subscribe("rfids/")
 client.subscribe("tables/")
